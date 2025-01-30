553ec425574ca7db38e1dda98835ec21c9e21e55
jberriol <jberriol@unal.edu.co>
2025-01-30 16:49:39 -0500

    commit sesion 18, forms

--- blog/tests.py
+++ blog/tests.py
@@ -45,3 +45,32 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Un buen título")
         self.assertTemplateUsed(response, "post_detail.html")
+
+    def test_post_createview(self):  # nuevo
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "Nuevo título",
+                "body": "Nuevo texto",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Nuevo título")
+        self.assertEqual(Post.objects.last().body, "Nuevo texto")
+
+    def test_post_updateview(self):  # nuevo
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Título actualizado",
+                "body": "Texto actualizado",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Título actualizado")
+        self.assertEqual(Post.objects.last().body, "Texto actualizado")
+
+    def test_post_deleteview(self):  # nuevo
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
